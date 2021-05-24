362b4d441cbdcd690c2994f4b27eda5afc539b64
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-05-24 07:43:39 +0000

    make Dockerfile and other files for use PostgreSQL

---
+++ postgres/README.md
@@ -0,0 +1,25 @@
+# PostgreSQLのtips
+## 関数定義について
+- [参考文献1](https://www.postgresql.jp/document/12/html/sql-createfunction.html)
+- [参考文献2](https://www.postgresql.jp/document/12/html/plpgsql-trigger.html)
+- PL/pgsqlでは、NEWはRECORDデータ型。
+```
+    この変数は行レベルのトリガでのINSERT/UPDATE操作によって更新された、新しいデータベースの行を保持します。
+    文レベルのトリガおよびDELETE操作では、この変数はnullです。
+```
+
+## 言語定義
+- [参考文献1](https://www.postgresql.jp/document/12/html/plpgsql-overview.html)
+- [参考文献2](https://www.postgresql.jp/document/12/html/sql-createlanguage.html)
+```
+    CREATE FUNCTION trigger_set_timestamp() RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+```
+- LANGUAGEで$$をplpgsqlとして扱えるようにしている
+
+## トリガーの作成
+- [参考文献1](https://www.postgresql.jp/document/12/html/sql-createtrigger.html)
---
+++ postgres/init.sql
@@ -0,0 +1,56 @@
+DROP TRIGGER IF EXISTS set_timestamp ON t_workspace;
+DROP TRIGGER IF EXISTS set_timestamp ON t_user;
+DROP TRIGGER IF EXISTS set_timestamp ON t_user_process;
+
+DROP FUNCTION IF EXISTS trigger_set_timestamp();
+DROP TABLE IF EXISTS t_workspace, t_user, t_user_process;
+
+CREATE TABLE t_workspace (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE t_user (
+    id varchar(255) PRIMARY KEY,
+    t_workspace_id int,
+    name varchar(255) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp,
+    FOREIGN KEY (t_workspace_id) REFERENCES t_workspace (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE t_user_process (
+    id serial PRIMARY KEY,
+    t_user_id varchar(255),
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp,
+    FOREIGN KEY (t_user_id) REFERENCES t_user (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE FUNCTION trigger_set_timestamp() RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON t_workspace
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON t_user
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON t_user_process
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
