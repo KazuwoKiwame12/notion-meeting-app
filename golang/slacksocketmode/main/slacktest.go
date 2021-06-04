8868fc96967e6ec39afdbfdbd488e68d49f541a0
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-05 01:28:40 +0900

    check the modal actions and repair code

---
+++ golang/slacksocketmode/main/slacktest.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"app/domain/function"
+	"app/domain/model"
+	"app/infrastructure"
+	"app/usecase"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+func main() {
+	// infrastructure初期化
+	sh, err := infrastructure.NewSqlHandler()
+	if err != nil {
+		log.Fatalf("newSqlHandler err: %+v", err)
+	}
+	defer func() {
+		if err := sh.DB.Close(); err != nil {
+			log.Fatalf("closed err: %+v", err)
+		}
+	}()
+
+	// domain初期化
+	dbOp := &function.DatabaseOperater{
+		SqlHandler: sh,
+	}
+
+	slackUC := &usecase.SlackUsecase{
+		DBOperator: dbOp,
+	}
+
+	webApi := slack.New(
+		os.Getenv("SLACK_TOKEN"),
+		slack.OptionAppLevelToken(os.Getenv("SLACK_SOCKET_TOKEN")),
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "api: ", log.Lshortfile|log.LstdFlags)),
+	)
+	socketMode := socketmode.New(
+		webApi,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "sm: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	go func() {
+		for envelope := range socketMode.Events {
+			switch envelope.Type {
+			case socketmode.EventTypeInteractive:
+				payload, _ := envelope.Data.(slack.InteractionCallback)
+				switch payload.Type {
+				case slack.InteractionTypeShortcut:
+					if payload.CallbackID == "notion-info__form" {
+						socketMode.Ack(*envelope.Request)
+						err = slackUC.GetModalView(payload.TriggerID)
+						if err != nil {
+							socketMode.Debugf("\nFailed to opemn a modal: %v\n", err)
+						}
+					}
+				case slack.InteractionTypeViewSubmission:
+					if payload.View.CallbackID == "notion-info__record" {
+						date, _ := strconv.Atoi(payload.View.State.Values["scheduler-date"]["static_select-action"].Value)
+						notion := model.Notion{
+							Date:              date,
+							NotionToken:       payload.View.State.Values["notion-token"]["plain_text_input-action"].Value,
+							NotionDatabaseID:  payload.View.State.Values["notion-database_id"]["plain_text_input-action"].Value,
+							NotionPageContent: payload.View.State.Values["notion-page_content"]["plain_text_input-action"].Value,
+						}
+						err = slackUC.RegisterNotionInfo(payload.Team.ID, payload.User.ID, notion)
+						if err != nil {
+							socketMode.Debugf("\nFailed to register notion info: %v\n", err)
+						}
+					}
+				default:
+					socketMode.Debugf("\nSkipped: %v\n", payload)
+				}
+			default:
+				socketMode.Debugf("\nSkipped: %v\n", envelope.Type)
+			}
+		}
+	}()
+	socketMode.Run()
+}
--- golang/usecase/slack.go
+++ golang/usecase/slack.go
@@ -7,31 +7,11 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"io/ioutil"
-
 	"github.com/slack-go/slack"
 )
 
 type SlackUsecase struct {
 	DBOperator *function.DatabaseOperater
-}
-
-func (su *SlackUsecase) GetModalView(triggerID string) error {
-	slackClient := slack.New(config.SLACK_TOKEN())
-	viewJson, err := ioutil.ReadFile("./modal.json")
-	if err != nil {
-		return fmt.Errorf("jsonファイル読み込みのエラー: %+v", err)
-	}
-	var viewObj slack.ModalViewRequest
-	if err := json.Unmarshal(viewJson, &viewObj); err != nil {
-		return fmt.Errorf("jsonファイルのデータを構造体にマウントウトする際のエラー: %+v", err)
-	}
-
-	if _, err := slackClient.OpenView(triggerID, viewObj); err != nil {
-		return fmt.Errorf("views.open api error: %+v", err)
-	}
-
-	return nil
 }
 
 func (su *SlackUsecase) RegisterNotionInfo(workspaceID, slackUserID string, notion model.Notion) error {
@@ -45,4 +25,173 @@ func (su *SlackUsecase) RegisterNotionInfo(workspaceID, slackUserID string, noti
 		return fmt.Errorf("register notion error: %+v", err)
 	}
 	return nil
+}
+
+func (su *SlackUsecase) GetModalView(triggerID string) error {
+	viewBytes := []byte(getViewWithJson())
+	var viewObj slack.ModalViewRequest
+	if err := json.Unmarshal(viewBytes, &viewObj); err != nil {
+		return fmt.Errorf("jsonファイルのデータを構造体にマウントウトする際のエラー: %+v", err)
+	}
+
+	slackClient := slack.New(config.SLACK_TOKEN())
+	if _, err := slackClient.OpenView(triggerID, viewObj); err != nil {
+		return fmt.Errorf("views.open api error: %+v", err)
+	}
+
+	return nil
+}
+
+func getViewWithJson() string {
+	return `
+	{
+		"callback_id": "notion-info__record",
+		"type": "modal",
+		"title": {
+			"type": "plain_text",
+			"text": "My App",
+			"emoji": true
+		},
+		"submit": {
+			"type": "plain_text",
+			"text": "Submit",
+			"emoji": true
+		},
+		"close": {
+			"type": "plain_text",
+			"text": "Cancel",
+			"emoji": true
+		},
+		"blocks": [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": "初めまして、このアプリは議事録フォーマットを定期的に自動作成するアプリです.\n実際に使用するためには以下の情報が必要です。\n1. notionのtoken\n2. notionのdatabase_id(議事録フォーマットを生成するページ)\n3. notionのpage_content(デフォルトで存在します)\n4. 議事録フォーマットを自動生成する曜日(デフォルトは水曜日です)\n\n *情報を入力してください*"
+				}
+			},
+			{
+				"type": "divider"
+			},
+			{
+				"type": "input",
+				"block_id": "notion-token",
+				"element": {
+					"type": "plain_text_input",
+					"action_id": "plain_text_input-action"
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "1. notionのtoken",
+					"emoji": true
+				}
+			},
+			{
+				"type": "input",
+				"block_id": "notion-database_id",
+				"element": {
+					"type": "plain_text_input",
+					"action_id": "plain_text_input-action"
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "2. notionのdatabase_id",
+					"emoji": true
+				}
+			},
+			{
+				"type": "input",
+				"block_id": "notion-page_content",
+				"element": {
+					"type": "plain_text_input",
+					"multiline": true,
+					"action_id": "plain_text_input-action"
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "3. notionのpage_content",
+					"emoji": true
+				}
+			},
+			{
+				"type": "input",
+				"block_id": "scheduler-date",
+				"element": {
+					"type": "static_select",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "曜日を選択してください",
+						"emoji": true
+					},
+					"options": [
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "月曜日",
+								"emoji": true
+							},
+							"value": "0"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "火曜日",
+								"emoji": true
+							},
+							"value": "1"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "水曜日",
+								"emoji": true
+							},
+							"value": "2"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "木曜日",
+								"emoji": true
+							},
+							"value": "3"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "金曜日",
+								"emoji": true
+							},
+							"value": "4"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "土曜日",
+								"emoji": true
+							},
+							"value": "5"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "日曜日",
+								"emoji": true
+							},
+							"value": "6"
+						}
+					],
+					"action_id": "static_select-action"
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "4. 議事録フォーマットを自動生成する曜日",
+					"emoji": true
+				}
+			},
+			{
+				"type": "divider"
+			}
+		]
+	}`
 }
