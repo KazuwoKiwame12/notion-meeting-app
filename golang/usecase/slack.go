619d8b12a3a6769738371c4b8d3a342a97e002c6
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-05 12:55:15 +0000

    replace the way of get modalview's json

--- golang/usecase/slack.go
+++ golang/usecase/slack.go
@@ -6,6 +6,7 @@ import (
 	"app/domain/model"
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/slack-go/slack"
 )
@@ -28,7 +29,11 @@ func (su *SlackUsecase) RegisterNotionInfo(workspaceID, slackUserID string, noti
 }
 
 func (su *SlackUsecase) GetModalView(triggerID string) error {
-	viewBytes := []byte(getViewWithJson())
+	jsonfilePath := fmt.Sprintf("%s/src/app/asset/slack/modalview.json", os.Getenv("GOPATH")) // 絶対パスで参照(相対パスの場合、当然だが実行ファイルからの相対パスとして認識される)
+	viewBytes, err := os.ReadFile(jsonfilePath)
+	if err != nil {
+		return fmt.Errorf("os.ReadFile error: %+v", err)
+	}
 	var viewObj slack.ModalViewRequest
 	if err := json.Unmarshal(viewBytes, &viewObj); err != nil {
 		return fmt.Errorf("jsonファイルのデータを構造体にマウントウトする際のエラー: %+v", err)
@@ -40,158 +45,4 @@ func (su *SlackUsecase) GetModalView(triggerID string) error {
 	}
 
 	return nil
-}
-
-func getViewWithJson() string {
-	return `
-	{
-		"callback_id": "notion-info__record",
-		"type": "modal",
-		"title": {
-			"type": "plain_text",
-			"text": "My App",
-			"emoji": true
-		},
-		"submit": {
-			"type": "plain_text",
-			"text": "Submit",
-			"emoji": true
-		},
-		"close": {
-			"type": "plain_text",
-			"text": "Cancel",
-			"emoji": true
-		},
-		"blocks": [
-			{
-				"type": "section",
-				"text": {
-					"type": "mrkdwn",
-					"text": "初めまして、このアプリは議事録フォーマットを定期的に自動作成するアプリです.\n実際に使用するためには以下の情報が必要です。\n1. notionのtoken\n2. notionのdatabase_id(議事録フォーマットを生成するページ)\n3. notionのpage_content(デフォルトで存在します)\n4. 議事録フォーマットを自動生成する曜日(デフォルトは水曜日です)\n\n *情報を入力してください*"
-				}
-			},
-			{
-				"type": "divider"
-			},
-			{
-				"type": "input",
-				"block_id": "notion-token",
-				"element": {
-					"type": "plain_text_input",
-					"action_id": "plain_text_input-action"
-				},
-				"label": {
-					"type": "plain_text",
-					"text": "1. notionのtoken",
-					"emoji": true
-				}
-			},
-			{
-				"type": "input",
-				"block_id": "notion-database_id",
-				"element": {
-					"type": "plain_text_input",
-					"action_id": "plain_text_input-action"
-				},
-				"label": {
-					"type": "plain_text",
-					"text": "2. notionのdatabase_id",
-					"emoji": true
-				}
-			},
-			{
-				"type": "input",
-				"block_id": "notion-page_content",
-				"element": {
-					"type": "plain_text_input",
-					"multiline": true,
-					"action_id": "plain_text_input-action"
-				},
-				"label": {
-					"type": "plain_text",
-					"text": "3. notionのpage_content",
-					"emoji": true
-				}
-			},
-			{
-				"type": "input",
-				"block_id": "scheduler-date",
-				"element": {
-					"type": "static_select",
-					"placeholder": {
-						"type": "plain_text",
-						"text": "曜日を選択してください",
-						"emoji": true
-					},
-					"options": [
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "月曜日",
-								"emoji": true
-							},
-							"value": "0"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "火曜日",
-								"emoji": true
-							},
-							"value": "1"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "水曜日",
-								"emoji": true
-							},
-							"value": "2"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "木曜日",
-								"emoji": true
-							},
-							"value": "3"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "金曜日",
-								"emoji": true
-							},
-							"value": "4"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "土曜日",
-								"emoji": true
-							},
-							"value": "5"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "日曜日",
-								"emoji": true
-							},
-							"value": "6"
-						}
-					],
-					"action_id": "static_select-action"
-				},
-				"label": {
-					"type": "plain_text",
-					"text": "4. 議事録フォーマットを自動生成する曜日",
-					"emoji": true
-				}
-			},
-			{
-				"type": "divider"
-			}
-		]
-	}`
 }
