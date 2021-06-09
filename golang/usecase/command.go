09202d2092330d3573a615b46ba66bc1636a75eb
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-09 17:23:21 +0000

    update text

--- golang/usecase/command.go
+++ golang/usecase/command.go
@@ -88,70 +88,152 @@ func (cu *CommandUsecase) Stop(userID int) {
 }
 
 func (cu *CommandUsecase) GetExplainMessage(name string) map[string]interface{} {
-	text := fmt.Sprintf("@%s\n", name) +
-		"## notion-meeting-app\n"+
-		"### できること\n"+
-		"1. notionに議事録のテンプレートページを定期的に自動作成できる\n"+
-		"2. 作成するテンプレートページの内容・場所・間隔を簡単にカスタマイズできる\n"+
-		"3. いつでも自動作成するのを停止できます。自動作成の軌道も同様に簡単にできる\n"+
-		"### 使い方\n"+
-		"1. ショートカット``Register the notion info`を選択する`\n"+
-		"2. 表示されたmodalの項目を埋める\n"+
-		"```"+
-		"1. notionのtoken\n"+
-		"2. notionのページを作成する場所\n"+
-		"3. notionのテンプレートページの内容\n"+
-		"4. テンプレートページを作成する曜日\n"+
-		"```\n\n"+
-		"3. ``/start``のslash commandでスケジューラを起動させる\n"+
-		"※スケジューラを停止させたい場合は``/stop``のslash commandで停止させる\n"+
-		"**上記のテンプレートページの内容の作り方**\n"+
-		"以下に例を示す。\n"+
-		"```"+
-		"title ミーティング\n"+
-		"heading_2 概要\n"+
-		"タイトル:\n\n"+
-		"heading_2 内容\n"+
-		"numbered_list_item sample\n\n"+
-		"numbered_list_item sample\n\n"+
-		"heading_2 次回\n"+
-		"次回の日程:\n"+
-		"書記: sample, リード: sample, 計測係: sample\n"+
-		"```\n\n"+
-		"1行目は、ページのタイトルに該当し、2行目以降は、ページの中身に該当する。\n"+
-		"番号付きリスト・todoリスト・リスト・heading1・heading2・heading3・テキスト・トグルなどのテキストタイプが使えます。\n"+
-		"上記を使うためには、以下の形式で記入する必要があります。\n"+
-		"```"+
-		"`テキストタイプ` `テキスト`..."+
-		"```\n\n"+
-		"以下にテキストタイプの種類を示す。\n"+
-		"- paragraph: 文章\n"+
-		"- heading_1: 1番大きいheader\n"+
-		"- heading_2: 2番大きいheader\n"+
-		"- heading_3: 3番大きいheader\n"+
-		"- bulleted_list_item: リスト\n"+
-		"- numbered_list_item: 番号つきリスト\n"+
-		"- to_do: todoリスト\n"+
-		"- toggle: toggleリスト\n"+
-		"ページ内容の例と実際のnotionのページの比較図を以下に掲載します。\n"+
-		"### コマンド一覧\n"+
-		"1. ``/explain``...アプリの使い方の説明を得る\n"+
-		"2. ショートカット``Register the notion info``...使用するnotion関連の情報を登録・更新する\n"+
-		"3. ``/start``...議事録のテンプレートページを定期的に自動作成するスケジューラを起動する\n"+
-		"4. ``/stop``...スケジューラを停止する\n"+
-		"5. ``/all``...スケジューラを起動しているユーザを確認する(admin用)\n"+
-		"6. ``/all-stop``...全てのスケジューラを停止する(admin用)\n"+
+	target := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: fmt.Sprintf("@%s\n", name),
+		},
+		nil,
+		nil,
+	)
+	headerCanDo := slack.NewHeaderBlock(
+		&slack.TextBlockObject{
+			Type: slack.PlainTextType,
+			Text: "このアプリでできること",
+		},
+	)
+	descriptionCanDo := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: "1. notionに議事録のテンプレートページを定期的に自動作成できる\n" +
+				"2. 作成するテンプレートページの内容・場所・間隔を簡単にカスタマイズできる\n" +
+				"3. いつでも自動作成するのを停止できます。自動作成の軌道も同様に簡単にできる\n",
+		},
+		nil,
+		nil,
+	)
+	headerHowToUse := slack.NewHeaderBlock(
+		&slack.TextBlockObject{
+			Type: slack.PlainTextType,
+			Text: "このアプリの使い方",
+		},
+	)
+	descriptionHowToUse := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: "1. ショートカット `Register the notion info`を選択する\n" +
+				"2. 表示されたmodalにある以下の項目を埋める\n" +
+				"```" +
+				"1. notionのtoken\n" +
+				"2. notionのページを作成する場所\n" +
+				"3. notionのテンプレートページの内容\n" +
+				"4. テンプレートページを作成する曜日\n" +
+				"```\n\n" +
+				"3. `/start`のslash commandでスケジューラを起動させる\n" +
+				"※スケジューラを停止させたい場合は `/stop`のslash commandで停止させる\n",
+		},
+		nil,
+		nil,
+	)
+	headerHowToCreatePage := slack.NewHeaderBlock(
+		&slack.TextBlockObject{
+			Type: slack.PlainTextType,
+			Text: "notion情報登録の際の、ページ内容の作り方",
+		},
+	)
+	descriptionHowToCreatePage := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: "1行目はページのタイトルに該当し、2行目以降はページの中身に該当する。\n" +
+				"文章・heading1・heading2・heading3・リスト・番号付きリスト・todoリスト・toggleリストなどのテキストタイプが使える。\n" +
+				"テキストタイプを変更する場合には `Enterキー`で改行する必要がある。\n" +
+				"ページ内容を記入する際には以下の形式である必要がある。\n" +
+				"```" +
+				"`テキストタイプ` `テキスト`..." +
+				"```\n\n" +
+				"以下にテキストタイプの種類を示す。\n",
+		},
+		[]*slack.TextBlockObject{
+			{
+				Type: "mrkdwn",
+				Text: "*text type*",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "*about*",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "`paragraph` or `空白`",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "文章",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "`heading_1`・ `heading_2`・ `heading_3`",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "1・2・3番大きいheader",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "`toggle`・ `bulleted_list_item`",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "toggleリスト・リスト",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "`to_do`・ `numbered_list_item`",
+			},
+			{
+				Type: "mrkdwn",
+				Text: "todoリスト・番号付きリスト",
+			},
+		},
+		nil,
+	)
+	descriptionExample := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: "以下にページ内容の登録例と実際のnotionの画面を示す。\n" +
+				"```" +
+				"title ミーティング\n" +
+				"heading_2 概要\n" +
+				"タイトル:\n\n" +
+				"heading_2 内容\n" +
+				"numbered_list_item sample\n\n" +
+				"numbered_list_item sample\n\n" +
+				"heading_2 次回\n" +
+				"次回の日程:\n" +
+				"書記: sample, リード: sample, 計測係: sample\n" +
+				"```\n",
+		},
+		nil,
+		nil,
+	)
+	img := slack.NewImageBlock(
+		"http://placekitten.com/500/500", // TODO 写真をnotionの画像に差し替える
+		"image",
+		"image",
+		nil,
+	)
 
 	msg := map[string]interface{}{
 		"blocks": []slack.Block{
-			slack.NewSectionBlock(
-				&slack.TextBlockObject{
-					Type: slack.MarkdownType,
-					Text: text,
-				},
-				nil,
-				nil,
-			),
+			target,
+			headerCanDo,
+			descriptionCanDo,
+			headerHowToUse,
+			descriptionHowToUse,
+			headerHowToCreatePage,
+			descriptionHowToCreatePage,
+			descriptionExample,
+			img,
 		},
 	}
 	return msg
