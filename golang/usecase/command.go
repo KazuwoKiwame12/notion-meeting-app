4bb2c77451bb0acd5659334d04d5718372106449
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-09 21:02:56 +0900

    repair text

--- golang/usecase/command.go
+++ golang/usecase/command.go
@@ -89,14 +89,58 @@ func (cu *CommandUsecase) Stop(userID int) {
 
 func (cu *CommandUsecase) GetExplainMessage(name string) map[string]interface{} {
 	text := fmt.Sprintf("@%s\n", name) +
-		"このアプリでは、notionに議事録のテンプレートページを定期的に自動生成するスケジューラを起動・停止することができます。\n" +
-		"スケジューラを動かすためには、以下の手順を行います。\n" +
-		"```1. ショートカット'Register the notion info'を選択し、表示されるモーダルにnotion情報を登録します。\n" +
-		"2. /startというslash commandを呼び出すことで、スケジューラが起動します。```\n\n" +
-		"スケジューラを停止させるためには、/stopを実行すればスケジューラは停止します。\n" +
-		"また、notion情報を更新する際には、再度'1'の手順を実行してください。\n" +
-		"※1ユーザにつき1スケジューラであるために、現時点では複数台のスケジューラを起動させることができません。" +
-		"そのような機能が必要であれば、管理人に連絡してください。"
+		"## notion-meeting-app\n"+
+		"### できること\n"+
+		"1. notionに議事録のテンプレートページを定期的に自動作成できる\n"+
+		"2. 作成するテンプレートページの内容・場所・間隔を簡単にカスタマイズできる\n"+
+		"3. いつでも自動作成するのを停止できます。自動作成の軌道も同様に簡単にできる\n"+
+		"### 使い方\n"+
+		"1. ショートカット``Register the notion info`を選択する`\n"+
+		"2. 表示されたmodalの項目を埋める\n"+
+		"```"+
+		"1. notionのtoken\n"+
+		"2. notionのページを作成する場所\n"+
+		"3. notionのテンプレートページの内容\n"+
+		"4. テンプレートページを作成する曜日\n"+
+		"```\n\n"+
+		"3. ``/start``のslash commandでスケジューラを起動させる\n"+
+		"※スケジューラを停止させたい場合は``/stop``のslash commandで停止させる\n"+
+		"**上記のテンプレートページの内容の作り方**\n"+
+		"以下に例を示す。\n"+
+		"```"+
+		"title ミーティング\n"+
+		"heading_2 概要\n"+
+		"タイトル:\n\n"+
+		"heading_2 内容\n"+
+		"numbered_list_item sample\n\n"+
+		"numbered_list_item sample\n\n"+
+		"heading_2 次回\n"+
+		"次回の日程:\n"+
+		"書記: sample, リード: sample, 計測係: sample\n"+
+		"```\n\n"+
+		"1行目は、ページのタイトルに該当し、2行目以降は、ページの中身に該当する。\n"+
+		"番号付きリスト・todoリスト・リスト・heading1・heading2・heading3・テキスト・トグルなどのテキストタイプが使えます。\n"+
+		"上記を使うためには、以下の形式で記入する必要があります。\n"+
+		"```"+
+		"`テキストタイプ` `テキスト`..."+
+		"```\n\n"+
+		"以下にテキストタイプの種類を示す。\n"+
+		"- paragraph: 文章\n"+
+		"- heading_1: 1番大きいheader\n"+
+		"- heading_2: 2番大きいheader\n"+
+		"- heading_3: 3番大きいheader\n"+
+		"- bulleted_list_item: リスト\n"+
+		"- numbered_list_item: 番号つきリスト\n"+
+		"- to_do: todoリスト\n"+
+		"- toggle: toggleリスト\n"+
+		"ページ内容の例と実際のnotionのページの比較図を以下に掲載します。\n"+
+		"### コマンド一覧\n"+
+		"1. ``/explain``...アプリの使い方の説明を得る\n"+
+		"2. ショートカット``Register the notion info``...使用するnotion関連の情報を登録・更新する\n"+
+		"3. ``/start``...議事録のテンプレートページを定期的に自動作成するスケジューラを起動する\n"+
+		"4. ``/stop``...スケジューラを停止する\n"+
+		"5. ``/all``...スケジューラを起動しているユーザを確認する(admin用)\n"+
+		"6. ``/all-stop``...全てのスケジューラを停止する(admin用)\n"+
 
 	msg := map[string]interface{}{
 		"blocks": []slack.Block{
