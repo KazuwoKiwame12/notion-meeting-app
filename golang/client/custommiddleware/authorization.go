720678678e6f3052d430dc46bbdad52c81923acf
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-24 17:21:55 +0000

    check requestbody

--- golang/client/custommiddleware/authorization.go
+++ golang/client/custommiddleware/authorization.go
@@ -25,6 +25,7 @@ func AuthUserMiddleware(auc *usecase.AuthorizationUsecase) echo.MiddlewareFunc {
 			if c.Request().Body != nil { // Read
 				reqBody, _ = ioutil.ReadAll(c.Request().Body)
 			}
+			log.Printf("reqBody:\n\t %+v\n", reqBody)
 			if err := verifySignature(c.Request().Header, reqBody, config.SLACK_SECRET()); err != nil {
 				log.Printf("error occured! in verify signature:\n\t %+v, %+v, %+v\n", c.Request().Header, reqBody, err)
 				return c.JSON(http.StatusBadRequest, err)
