1e97c6b199dbe7ce674df1e65442b9cb42864322
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-06-27 10:24:44 +0000

    repair authorization

--- golang/client/custommiddleware/authorization.go
+++ golang/client/custommiddleware/authorization.go
@@ -4,47 +4,20 @@ import (
 	"app/config"
 	"app/usecase"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"strconv"
+	"net/url"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/slack-go/slack"
 )
 
-const (
-	failed string = ""
-)
-
 func AuthUserMiddleware(auc *usecase.AuthorizationUsecase) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			reqBody := []byte{}
-			if c.Request().Body != nil { // Read
-				reqBody, _ = ioutil.ReadAll(c.Request().Body)
-			}
-			log.Printf("reqBody: %+v\n", string(reqBody))
-			if err := verifySignature(c.Request().Header, reqBody, config.SLACK_SECRET()); err != nil {
-				log.Printf("error occured! in verify signature:\n\t %+v, %+v, %+v\n", c.Request().Header, reqBody, err)
-				return c.JSON(http.StatusBadRequest, err)
-			}
-
-			workspaceID, slackUserID, err := getWorkspaceIDAndSlackUserID(c.FormValue("type"), c.FormValue("payload"))
-			if err != nil {
-				log.Printf("error occured! in getWorkspaceIDAndSlackUserID:\n\t %+v, %+v, %+v\n", workspaceID, slackUserID, err)
-				return c.JSON(http.StatusBadRequest, err)
-			}
-			userID, userName, err := auc.IsUser(workspaceID, slackUserID)
-			if err != nil {
-				log.Printf("error occured! in auc.IsUser:\n\t %+v, %+v, %+v\n", userID, userName, err)
-				return c.JSON(http.StatusBadRequest, err)
-			}
-			c.Request().Form.Set("user_id", strconv.Itoa(userID))
-			c.Request().Form.Set("user_name", userName)
-			log.Printf("form_values: %+v\n", c.Request().Form)
-			return next(c)
+			return authFunc(c, next, auc.IsUser)
 		}
 	}
 }
@@ -52,23 +25,71 @@ func AuthUserMiddleware(auc *usecase.AuthorizationUsecase) echo.MiddlewareFunc {
 func AuthAdminMiddleware(auc *usecase.AuthorizationUsecase) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			if err := verifySignature(c.Request().Header, []byte(c.FormValue("payload")), config.SLACK_SECRET()); err != nil {
-				return c.JSON(http.StatusBadRequest, err)
-			}
-
-			workspaceID, slackUserID, err := getWorkspaceIDAndSlackUserID(c.FormValue("type"), c.FormValue("payload"))
-			if err != nil {
-				return c.JSON(http.StatusBadRequest, err)
-			}
-			userID, userName, err := auc.IsAdmin(workspaceID, slackUserID)
-			if err != nil {
-				return c.JSON(http.StatusBadRequest, err)
-			}
-			c.Request().Form.Set("user_id", strconv.Itoa(userID))
-			c.Request().Form.Set("user_name", userName)
-			return next(c)
+			return authFunc(c, next, auc.IsAdmin)
 		}
 	}
+}
+
+func authFunc(c echo.Context, next echo.HandlerFunc, userAuthFunc func(string, string) (int, string, error)) error {
+	reqBody := []byte{}
+	if c.Request().Body != nil { // Read
+		reqBody, _ = ioutil.ReadAll(c.Request().Body)
+	}
+
+	if err := verifySignature(c.Request().Header, reqBody, config.SLACK_SECRET()); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	workspaceID, slackUserID, payload, err := getIDsAndPayload(reqBody)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	userID, userName, err := userAuthFunc(workspaceID, slackUserID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	c.Request().Form = url.Values{
+		"user_id":   {fmt.Sprintf("%d", userID)},
+		"user_name": {userName},
+		"payload":   {payload},
+	}
+	return next(c)
+}
+
+func getIDsAndPayload(reqBody []byte) (string, string, string, error) {
+	reqbodyString, err := url.QueryUnescape(string(reqBody))
+	if err != nil {
+		return "", "", "", err
+	}
+
+	if reqbodyString[0:8] == "payload=" {
+		// interactive message
+		var interactionObj slack.InteractionCallback
+
+		if err := json.Unmarshal([]byte(reqbodyString[8:]), &interactionObj); err != nil {
+			return "", "", "", err
+		}
+		return interactionObj.User.TeamID, interactionObj.User.ID, reqbodyString[8:], nil
+	}
+
+	//slash commands
+	bodyContents := strings.Split(reqbodyString, "&")
+	const (
+		indexValue  int = 1
+		indexTeamID int = 1
+		indexUserID int = 5
+	)
+	teamIDContent, userIDContent := strings.Split(bodyContents[indexTeamID], "="), strings.Split(bodyContents[indexUserID], "=")
+	workspaceID, err := url.QueryUnescape(teamIDContent[indexValue])
+	if err != nil {
+		return "", "", "", err
+	}
+	slackUserID, err := url.QueryUnescape(userIDContent[indexValue])
+	if err != nil {
+		return "", "", "", err
+	}
+	return workspaceID, slackUserID, "", nil
 }
 
 /*リクエストがslackから送られてきたものであることの保証する関数*/
@@ -83,24 +104,4 @@ func verifySignature(header http.Header, body []byte, secret string) error {
 	}
 
 	return nil
-}
-
-func getWorkspaceIDAndSlackUserID(requestType, payload string) (string, string, error) {
-	switch requestType {
-	case "slash_commands":
-		var obj *slack.SlashCommand
-		if err := json.Unmarshal([]byte(payload), obj); err != nil {
-			return failed, failed, err
-		}
-		return obj.TeamID, obj.UserID, nil
-	case "shortcut":
-		var obj *slack.InteractionCallback
-		if err := json.Unmarshal([]byte(payload), obj); err != nil {
-			return failed, failed, err
-		}
-		return obj.User.TeamID, obj.User.ID, nil
-	default:
-		log.Println("invalid request type")
-		return failed, failed, errors.New("invalid request type")
-	}
 }
